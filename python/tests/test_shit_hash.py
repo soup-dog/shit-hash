4f9f25ad3c777b007518642ee13ad0578acd19ea
Conall Barry <80365234+AConcrnedCitizen@users.noreply.github.com>
2025-03-11 23:11:23 +1100

    Update tests

--- python/tests/test_shit_hash.py
+++ python/tests/test_shit_hash.py
@@ -1,12 +1,12 @@
 from shit_hash import *
 
-
 def test_shit_hash_string():
-    assert shit_hash("foo") == 42
+    assert shit_hash(b"foo") == 42
 
 
 def test_shit_hash_int():
-    assert shit_hash(1) == 42
+    a = 1
+    assert shit_hash(a.to_bytes()) == 42
 
 
 def test_shit_hash_func():
