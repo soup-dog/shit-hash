be9f9545eadf0f159a179ab0a1a893866feb6a8b
Conall Barry <80365234+AConcrnedCitizen@users.noreply.github.com>
2025-03-11 23:11:36 +1100

    Update shit_hash.py

--- python/shit_hash/shit_hash.py
+++ python/shit_hash/shit_hash.py
@@ -1,3 +1,13 @@
-def shit_hash(data: bytes) -> int: 
+from random import randint
+from types import FunctionType
+
+def shit_hash(data: bytes = None) -> int: 
+    if data == None:
+        data = randint(0, 999999)    
+    
+    data = randint(0, 999999) if isinstance(data, FunctionType) else bytes(data) if not isinstance(data, bytes) else data
+    
     thing = int.from_bytes(bytearray(data))
-    return (thing*0)*42
+    return (thing**0)*42
+
+print(shit_hash())
