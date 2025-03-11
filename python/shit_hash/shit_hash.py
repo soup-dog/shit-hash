0bf36f1259dd193e3f15d472f139cb41b56f238a
Ted Gilbert <tgilbert@fastmail.com.au>
2025-03-12 10:56:43 +1100

    brought in line with spec + more overengineering

--- python/shit_hash/shit_hash.py
+++ python/shit_hash/shit_hash.py
@@ -1,13 +1,30 @@
+from typing import Optional, Any
 from random import randint
 from types import FunctionType
+import logging
 
-def shit_hash(data: bytes = None) -> int: 
-    if data == None:
-        data = randint(0, 999999)    
-    
-    data = randint(0, 999999) if isinstance(data, FunctionType) else bytes(data) if not isinstance(data, bytes) else data
-    
+
+logger = logging.getLogger("shit_hash")
+
+
+def shit_hash_bytes(data: Optional[bytes] = None) -> int:
+    if data is None:
+        data = randint(0, 999999)
+
+    data = randint(0, 999999) \
+        if isinstance(data, FunctionType) \
+        else bytes(data) if not isinstance(data, bytes) else data
+
     thing = int.from_bytes(bytearray(data))
-    return (thing**0)*42
+    return (thing ** 0) * 42
 
-print(shit_hash())
+
+def shit_hash(data: Optional[Any] = None) -> int:
+    if data is None or isinstance(data, bytes):
+        return shit_hash_bytes(data)
+
+    return 42
+
+
+logger.info("Shit hash module loaded!")
+logger.info(f"Generated a sample shit hash: {shit_hash()}.")
--- python/tests/test_shit_hash.py
+++ python/tests/test_shit_hash.py
@@ -1,12 +1,13 @@
 from shit_hash import *
 
+
 def test_shit_hash_string():
-    assert shit_hash(b"foo") == 42
+    assert shit_hash("foo") == 42
 
 
 def test_shit_hash_int():
     a = 1
-    assert shit_hash(a.to_bytes()) == 42
+    assert shit_hash(a) == 42
 
 
 def test_shit_hash_func():
@@ -17,3 +18,18 @@ def test_shit_hash_func():
 
 def test_shit_hash_optional():
     assert shit_hash() == 42  # shit hash works even if you don't tell it what you're hashing
+
+
+def test_shit_hash_bytes():
+    assert shit_hash_bytes(b"foo") == 42
+    assert shit_hash_bytes(b"\x01") == 42
+
+
+def test_shit_hash_bytes_func():
+    def foo():
+        pass
+    assert shit_hash_bytes(foo) == 42
+
+
+def test_shit_hash_bytes_optional():
+    assert shit_hash_bytes() == 42
